e9158fcd479aa4ddd98263a78e6a9aa1e2e29598
Luca Rizzi <lrizzi@keck.hawaii.edu>
2019-04-07 09:18:57 -1000

    Adding exception for lris and adding handling of + in instrumenet names

--- services/getSchedules.py
+++ services/getSchedules.py
@@ -51,6 +51,8 @@ def getNightStaff(inDate):
 
     return json.loads(result)
 
+
+
 def genLogs( d, db, errors ):
     """ Parses out database log entries from the schedule for a given date """
 
@@ -98,6 +100,9 @@ def genLogs( d, db, errors ):
                 run['Instrument'] = run['Instrument'].split('-')[0]
             if 'HIRES' in run['Instrument']:
                 run['Instrument'] = 'hires'
+            if 'LRIS' in run['Instrument']:
+                run['Instrument'] = 'lris'
+
 
             curInstrument = db.instruments.find_one({'name': run['Instrument'].upper()})
             if curInstrument is not None:
@@ -126,3 +131,92 @@ def genLogs( d, db, errors ):
     # Return the log entries generated
     return outLogs
 
+def genLogs_multiple_instruments( d, db, errors ):
+    """ Parses out database log entries from the schedule for a given date """
+
+    # An empty list to append output logs
+    outLogs = []
+
+    # List of months to be used for really lame lower casing method
+    mons = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+    night_staff = getNightStaff(d)
+
+    suffixes = ['', '_B', '_C', '_D', '_E']
+    instruments_used_tonight = []
+
+    employees = getNightStaff(d)
+
+    for telescope in [1, 2]:
+        suffix_index = 0
+        schedules = getSchedule(d, telescope)
+        for run in schedules:
+            # array containing the log for this telescope
+            logs = []
+
+            project = run['ProjCode']
+            observers = run['Observers']
+            pi = run['PiLastName']
+
+            # use default values to prevent failures when either the OA or teh SA are not specified
+            oa = sa = 'UNKNOWN'
+
+            # retrieve OA name
+            for staff in filter(
+                    lambda nightstaff: (('oa' in nightstaff['Type']) and (nightstaff['TelNr'] == str(telescope))),
+                    employees):
+                oa = staff['FirstName'] + " " + staff['LastName']
+            # retrieve SA name
+            for staff in filter(
+                    lambda nightstaff: (('sa' in nightstaff['Type']) and (nightstaff['TelNr'] == str(telescope))),
+                    employees):
+                sa = staff['FirstName'] + " " + staff['LastName']
+
+            utcDate = datetime.utcnow()
+            # retrieve protodirs structure
+            # it is possible to have more than 1 instrument per run, separated by the + sign
+            instruments = run['Instrument'].split('+')
+            for index in range(len(instruments)):
+                print("Looking for database entry for instrument: <%s>" % instruments[index])
+                if '-' in instruments[index]:
+                    instruments[index] = instruments[index].split('-')[0]
+                if 'HIRES' in instruments[index]:
+                    instruments[index] = 'hires'
+                if 'LRIS' in instruments[index]:
+                    instruments[index] = 'lris'
+            
+            # now loops through the instruments for this run, and generate the logs
+            for instrument in instruments:
+                log = {}
+                curInstrument = db.instruments.find_one({'name': instrument.upper()})
+                if curInstrument is not None:
+                    log_instrument = instrument.upper()
+                    obs = {}
+                    obs['date'] = d + timedelta(days=1)
+                    obs['accountID'] = "".join([str(s) for s in run['Account'] if s.isdigit()])
+                    instruments_used_tonight.append(instrument)
+                    number_of_occurrences_of_this_instrument = instruments_used_tonight.count(instrument)
+                    suffix_index = number_of_occurrences_of_this_instrument-1
+                    obs['dateSuffix'] = suffixes[suffix_index]
+
+                    suffix_index += 1
+                    dataDirs = []
+                    for pd in curInstrument["protoDirs"]:
+                        dir = pd.format(**obs)
+                        for m in mons:
+                            if m in dir:
+                                dir = dir.replace(m, m.lower())
+                        dataDirs.append(dir)
+                    log['dataDirs'] = dataDirs
+                    log['activeDirs'] = []
+                    log['sa'] = sa
+                    log['oa'] = oa
+                    log['project'] = project
+                    log['observers'] = observers
+                    log['pi'] = pi
+                    log['instrument'] = instrument
+                    outLogs.append(log)
+                else:
+                    print("No instrument entry for <%s>" % (run['Instrument'].upper()))
+    # Return the log entries generated
+    return outLogs
